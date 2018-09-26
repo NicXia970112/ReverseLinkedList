2c06b7bcb5e4cad394c40a81c36acd76f7989761
nicxia001 <nicxia001@gmail.com>
2018-09-26 20:00:01 +0800

    测试

---
+++ Solution.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        List l = new List();
+        l.insert(node1);
+        l.insert(node2);
+        l.insert(node3);
+        l.insert(node4);
+        l.insert(node5);
+        l.print();
+        System.out.println();
+        List newList = l.reverse();
+        newList.print();
+
+    }
+
+}
+
+class Node {
+
+    public Node next;
+
+    public int val;
+
+    Node(int value) {
+        this.val = value;
+    }
+
+}
+
+class List {
+
+    public Node head;
+
+    public Node tail;
+
+    public int size = 0;
+
+    public void print() {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.val + "   ");
+            p = p.next;
+        }
+    }
+
+    public void insert(Node n) {
+        if(head == null) {
+            head = n;
+        }
+        else {
+            n.next = head;
+            head = n;
+        }
+    }
+
+    public List reverse() {
+        Node p = head;
+        Node newHead = p.next;
+        List l = new List();
+        while (newHead != null) {
+            p.next = null;
+            l.insert(p);
+            p = newHead;
+            newHead = newHead.next;
+            if(newHead == null) {
+                p.next = null;
+                l.insert(p);
+            }
+        }
+        return l;
+    }
+
+
+
+}
